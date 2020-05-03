9f7995f6a05f9b9a9184da1a20be5fa3c7228892
Leonardo Romor <leonardo.romor@gmail.com>
2020-05-03 18:58:48 +0200

    Added missing sim and Makefile

---
+++ sim/sim_main.cc
@@ -0,0 +1,22 @@
+
+#include "BeagleSpi.h"
+#include "verilated.h"
+#include <iostream>
+#include "BeagleSpi__Dpi.h"
+
+void trigger(unsigned char clki, unsigned char *a) {
+   // Immediately trigger
+   std::cout << "CYCLE!" << std::endl;
+   *a = 1;
+}
+
+int main(int argc, char** argv, char** env) {
+    Verilated::commandArgs(argc, argv);
+    BeagleSpi * top = new BeagleSpi;
+    while (!Verilated::gotFinish()) {
+        ~top->clki;
+        top->eval();
+    }
+    delete top;
+    return 0;
+}
