af3752830d19f8d7e85190abd9182dfd7e9ea813
Henner Zeller <h.zeller@acm.org>
2020-06-14 14:29:30 -0700

    Add diagnostic output of which spi device is used.

--- host/beagleg-protocol.cc
+++ host/beagleg-protocol.cc
@@ -126,10 +126,11 @@ int main(int argc, char *argv[]) {
 
     SPIHost spi;
     if (!spi.Connect(device, options)) {
-        fprintf(stderr, "Couldn't connect to SPI bus\n");
+        fprintf(stderr, "Couldn't connect to SPI bus %s\n", device);
         return 1;
     }
 
+    printf("Using %s\n", device);
     printf("Assuming\n\tsizeof(MotionSegment) = %d\n\tsizeof(QueueStatus) = %d\n\n",
            (int)sizeof(beagleg::MotionSegment),
            (int)sizeof(beagleg::QueueStatus));
