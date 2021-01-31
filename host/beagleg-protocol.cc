3b6f680b81ab5974271cafe65d466e1c19d06c62
Henner Zeller <h.zeller@acm.org>
2021-01-31 10:58:54 -0800

    Hack up beagleg protocol to use simulation.

--- host/beagleg-protocol.cc
+++ host/beagleg-protocol.cc
@@ -9,15 +9,17 @@
 
 #include <algorithm>
 
+#include "../sim/hsg-sim.h"
+
 // The protocol elements. Right now just pretend
 namespace beagleg {
 struct MotionSegment {
-    uint32_t count_steps;    // see beagleg/src/motion-queue.h for real deal.
+  uint32_t count_steps;    // see beagleg/src/motion-queue.h for real deal.
 };
 
 struct QueueStatus {
-    uint32_t counter : 24; // remaining number of cycles to be performed
-    uint32_t index : 8;    // represent the executing slot [0 to QUEUE_LEN - 1]
+  uint32_t counter : 24; // remaining number of cycles to be performed
+  uint32_t index : 8;    // represent the executing slot [0 to QUEUE_LEN - 1]
 };
 }  // namespace beagleg
 
@@ -29,217 +31,220 @@ static constexpr int BYTES_PER_FIFO_RECORD = sizeof(beagleg::MotionSegment);
 
 // This needs to be in-sync with FPGA impl.
 enum Command {
-    CMD_NO_OP      = 0x00,  // Just send one byte to receive fifo free
-    CMD_STATUS     = 0x01, // Send 5 bytes, receive fifo free + status word
-    CMD_WRITE_FIFO = 0x02,
-    CMD_READ_FIFO  = 0x03,
+  CMD_NO_OP      = 0x00,  // Just send one byte to receive fifo free
+  CMD_STATUS     = 0x01, // Send 5 bytes, receive fifo free + status word
+  CMD_WRITE_FIFO = 0x02,
+  CMD_READ_FIFO  = 0x03,
 };
 
 // Put terminal in canonical mode, read chars.
 class TerminalInput {
 public:
-    TerminalInput() {
-        if (tcgetattr(0, &orig_state_)){
-            fprintf(stderr, "Error getting current terminal settings\n");
-            return;  // shrug.
-        }
-
-        termios modified_tios = orig_state_;
-
-        modified_tios.c_lflag &= ~ICANON;   // Get each character.
-        modified_tios.c_lflag &= ~ECHO;     // Don't want to echo
-
-        if (tcsetattr(0, TCSANOW, &modified_tios)) {
-            printf("Uh, couldn't set terminal settings\n");
-        }
+  TerminalInput() {
+    if (tcgetattr(0, &orig_state_)){
+      fprintf(stderr, "Error getting current terminal settings\n");
+      return;  // shrug.
     }
 
-    ~TerminalInput() {
-        // Reset to previous state.
-        tcsetattr(0, TCSANOW, &orig_state_);
-    }
+    termios modified_tios = orig_state_;
 
-    char read_char() {
-        int c;
-        read(STDIN_FILENO, &c, 1);
-        return c;
+    modified_tios.c_lflag &= ~ICANON;   // Get each character.
+    modified_tios.c_lflag &= ~ECHO;     // Don't want to echo
+
+    if (tcsetattr(0, TCSANOW, &modified_tios)) {
+      printf("Uh, couldn't set terminal settings\n");
     }
+  }
+
+  ~TerminalInput() {
+    // Reset to previous state.
+    tcsetattr(0, TCSANOW, &orig_state_);
+  }
+
+  char read_char() {
+    int c;
+    read(STDIN_FILENO, &c, 1);
+    return c;
+  }
 
 private:
-    termios orig_state_;
+  termios orig_state_;
 };
 
 class BeagleGSPIProtocol {
 public:
-    BeagleGSPIProtocol(SPIHost *channel) : spi_channel_(channel) {}
+  BeagleGSPIProtocol(SPIHost *channel) : spi_channel_(channel) {}
 
-    // Get number of free slots.
-    int GetFreeSlots() {
-        const char cmd = CMD_NO_OP;
-        char val;
-        spi_channel_->TransferBuffer(&cmd, &val, 1);
-        return val;
+  // Get number of free slots.
+  int GetFreeSlots() {
+    const char cmd = CMD_NO_OP;
+    char val;
+    spi_channel_->TransferBuffer(&cmd, &val, 1);
+    return val;
+  }
+
+  // Get queue status. Also return number of free slots.
+  int GetQueueStatus(beagleg::QueueStatus *status) {
+    const size_t tx_len = 1 + sizeof(beagleg::QueueStatus);
+    char tx_buffer[tx_len] = {};
+    char rx_buffer[tx_len];
+    tx_buffer[0] = CMD_STATUS;
+    spi_channel_->TransferBuffer(tx_buffer, rx_buffer, tx_len);
+    memcpy(status, rx_buffer + 1, sizeof(*status));
+    return rx_buffer[0];
+  }
+
+  // Attempts to send motion segments. Only sends amount possible.
+  // Returns number of segments sent.
+  int SendMotionSegments(const beagleg::MotionSegment *segments, int count) {
+    fprintf(stderr, "Writing first byte to read free slots, and but keep CS low to continue transaction...\n");
+    const char command = CMD_WRITE_FIFO;
+    uint8_t free_slots;
+    if (!spi_channel_->TransferBuffer(&command, &free_slots, 1, false))
+      return -1;
+
+    if (free_slots < count) {
+      fprintf(stderr, "Available fifo space of %d < requested %d\n",
+              free_slots, count);
     }
 
-    // Get queue status. Also return number of free slots.
-    int GetQueueStatus(beagleg::QueueStatus *status) {
-        const size_t tx_len = 1 + sizeof(beagleg::QueueStatus);
-        char tx_buffer[tx_len] = {};
-        char rx_buffer[tx_len];
-        tx_buffer[0] = CMD_STATUS;
-        spi_channel_->TransferBuffer(tx_buffer, rx_buffer, tx_len);
-        memcpy(status, rx_buffer + 1, sizeof(*status));
-        return rx_buffer[0];
-    }
-
-    // Attempts to send motion segments. Only sends amount possible.
-    // Returns number of segments sent.
-    int SendMotionSegments(const beagleg::MotionSegment *segments, int count) {
-        fprintf(stderr, "Writing first byte to read free slots, and but keep CS low to continue transaction...\n");
-        const char command = CMD_WRITE_FIFO;
-        uint8_t free_slots;
-        if (!spi_channel_->TransferBuffer(&command, &free_slots, 1, false))
-            return -1;
-
-        if (free_slots < count) {
-            fprintf(stderr, "Available fifo space of %d < requested %d\n",
-                    free_slots, count);
-        }
-
-        const int segment_tx_count = std::min((int)free_slots, count);
-        const int segment_byte_len = segment_tx_count * sizeof(beagleg::MotionSegment);
-        char rx_buffer[segment_byte_len];   // Let's see what it sends back
-        fprintf(stderr, "Sending actual data; %d elements = %d bytes\n",
-                segment_tx_count, segment_byte_len);
-        if (!spi_channel_->TransferBuffer(segments, rx_buffer, segment_byte_len))
-            return -1;
-        return segment_tx_count;
-    }
+    const int segment_tx_count = std::min((int)free_slots, count);
+    const int segment_byte_len = segment_tx_count * sizeof(beagleg::MotionSegment);
+    char rx_buffer[segment_byte_len];   // Let's see what it sends back
+    fprintf(stderr, "Sending actual data; %d elements = %d bytes\n",
+            segment_tx_count, segment_byte_len);
+    if (!spi_channel_->TransferBuffer(segments, rx_buffer, segment_byte_len))
+      return -1;
+    return segment_tx_count;
+  }
 
 private:
-    SPIHost *const spi_channel_;
+  SPIHost *const spi_channel_;
 };
 
 
 static void print_free_slots(int slots) {
-    printf("Free slots: %d\n", slots);
+  printf("Free slots: %d\n", slots);
 }
 
 static void print_status(const beagleg::QueueStatus &status) {
-    printf("Status: counter:%d; index:%d\n", status.counter, status.index);
+  printf("Status: counter:%d; index:%d\n", status.counter, status.index);
 }
 
 static void ReadSegmentSteps(TerminalInput *terminal, beagleg::MotionSegment *segment) {
-    printf("How many steps: ");
-    fflush(stdout);
-    char k;
+  printf("How many steps: ");
+  fflush(stdout);
+  char k;
 
-    segment->count_steps = 0;
-    while ((k = terminal->read_char()) != '\n') {
-        if (k >= '0' && k <= '9') {
-            write(STDOUT_FILENO, &k, 1);
-            segment->count_steps = segment->count_steps * 10 + (k - '0');
-        } else {
-            k = '\007';  // beep.
-            write(STDOUT_FILENO, &k, 1);
-        }
+  segment->count_steps = 0;
+  while ((k = terminal->read_char()) != '\n') {
+    if (k >= '0' && k <= '9') {
+      write(STDOUT_FILENO, &k, 1);
+      segment->count_steps = segment->count_steps * 10 + (k - '0');
+    } else {
+      k = '\007';  // beep.
+      write(STDOUT_FILENO, &k, 1);
     }
-    printf(" \u2713\n");
+  }
+  printf(" \u2713\n");
 }
 
 static void SendSegment(const beagleg::MotionSegment &segment, BeagleGSPIProtocol *protocol) {
-    const int segments_written = protocol->SendMotionSegments(&segment, 1);
-    if (segments_written) {
-        // Also write it in hex to better see the endianness we send over the wire.
-        printf("Wrote segment with %d steps (that is 0x%02X·%02X·%02X·%02X hex)\n",
-               segment.count_steps,
-               (segment.count_steps >> 24) & 0xFF,
-               (segment.count_steps >> 16) & 0xFF,
-               (segment.count_steps >> 8) & 0xFF,
-               (segment.count_steps >> 0) & 0xFF);
-    } else {
-        printf("FIFO full. Didn't write segment\n");
-    }
+  const int segments_written = protocol->SendMotionSegments(&segment, 1);
+  if (segments_written) {
+    // Also write it in hex to better see the endianness we send over the wire.
+    printf("Wrote segment with %d steps (that is 0x%02X·%02X·%02X·%02X hex)\n",
+           segment.count_steps,
+           (segment.count_steps >> 24) & 0xFF,
+           (segment.count_steps >> 16) & 0xFF,
+           (segment.count_steps >> 8) & 0xFF,
+           (segment.count_steps >> 0) & 0xFF);
+  } else {
+    printf("FIFO full. Didn't write segment\n");
+  }
 }
 
 
 int main(int argc, char *argv[]) {
-    const char *device = "/dev/spidev0.0";
-    int opt;
+  const bool use_simulation = true;  // TODO: make flag.
+  const char *device = "/dev/spidev0.0";
+  int opt;
 
-    while ((opt = getopt(argc, argv, "D:")) != -1) {
-        switch (opt) {
-        case 'D':
-            device = strdup(optarg);
-            break;
-        }
+  while ((opt = getopt(argc, argv, "D:")) != -1) {
+    switch (opt) {
+    case 'D':
+      device = strdup(optarg);
+      break;
     }
+  }
 
-    SPIHost::Options options;
-    options.bits_per_word = 8;
-    options.speed_hz = 500'000;  // Let's be slow for now
-    options.verbose = true;
+  SPIHost::Options options;
+  options.bits_per_word = 8;
+  options.speed_hz = 500'000;  // Let's be slow for now
+  options.verbose = true;
 
-    SPIHost spi;
-    if (!spi.Connect(device, options)) {
-        fprintf(stderr, "Couldn't connect to SPI bus %s (change with -D)\n", device);
-        return 1;
+  SPIHost spi(use_simulation
+              ? StepGeneratorModuleSim::Init(argc, argv)
+              : nullptr);
+  if (!spi.Connect(device, options)) {
+    fprintf(stderr, "Couldn't connect to SPI bus %s (change with -D)\n", device);
+    return 1;
+  }
+
+  printf("Using %s (change with -D)\n", device);
+  printf("Assuming\n\tsizeof(MotionSegment) = %d\n\tsizeof(QueueStatus) = %d\n\n",
+         (int)sizeof(beagleg::MotionSegment),
+         (int)sizeof(beagleg::QueueStatus));
+
+  printf("User interaction:\n"
+         "\tf    - no-op. Just read fifo free slots\n"
+         "\ts    - read status word (fifo free + status)\n"
+         "\tw    - read step count and send motion segment to fifo\n"
+         "\tW    - send motion segment to fifo with previously with lower-case 'w' read steps.\n"
+         "\tESC  - quit\n"
+         );
+
+  static constexpr char nothing_to_do[] = "¯\\_(ツ)_/¯";
+
+  beagleg::MotionSegment segment;
+  segment.count_steps = 3;
+  beagleg::QueueStatus status;
+  BeagleGSPIProtocol protocol(&spi);
+  TerminalInput terminal;
+
+  for (;;) {
+    fprintf(stderr, "------[ f: free-slots, s: status; w: write; W: write same again; ESC: quit] --------------\n");
+    const char key = terminal.read_char();
+    switch (key) {
+    case 'f':
+    case ' ':   // Also space key is a wonderful way to send no-op.
+      print_free_slots(protocol.GetFreeSlots());
+      break;
+
+    case 's':  // Read status
+      print_free_slots(protocol.GetQueueStatus(&status));
+      print_status(status);
+      break;
+
+    case 'w':
+      ReadSegmentSteps(&terminal, &segment);
+      SendSegment(segment, &protocol);
+      break;
+
+    case 'W':
+      // Send previously read segment.
+      SendSegment(segment, &protocol);
+      break;
+
+    case 'q':
+    case 'Q':
+    case 0x04:  // Ctrl-D
+    case 0x1b:  // Escape
+      fprintf(stderr, "Signing off; Have a wonderful day!\n");
+      return 0;
+
+    default:
+      fprintf(stderr, "input key \e[1;31m0x%02X\e[0m: \e[1;33m%s\e[0m\n", key, nothing_to_do);
     }
-
-    printf("Using %s (change with -D)\n", device);
-    printf("Assuming\n\tsizeof(MotionSegment) = %d\n\tsizeof(QueueStatus) = %d\n\n",
-           (int)sizeof(beagleg::MotionSegment),
-           (int)sizeof(beagleg::QueueStatus));
-
-    printf("User interaction:\n"
-           "\tf    - no-op. Just read fifo free slots\n"
-           "\ts    - read status word (fifo free + status)\n"
-           "\tw    - read step count and send motion segment to fifo\n"
-           "\tW    - send motion segment to fifo with previously with lower-case 'w' read steps.\n"
-           "\tESC  - quit\n"
-        );
-
-    static constexpr char nothing_to_do[] = "¯\\_(ツ)_/¯";
-
-    beagleg::MotionSegment segment;
-    segment.count_steps = 3;
-    beagleg::QueueStatus status;
-    BeagleGSPIProtocol protocol(&spi);
-    TerminalInput terminal;
-
-    for (;;) {
-        fprintf(stderr, "------[ f: free-slots, s: status; w: write; W: write same again; ESC: quit] --------------\n");
-        const char key = terminal.read_char();
-        switch (key) {
-        case 'f':
-        case ' ':   // Also space key is a wonderful way to send no-op.
-            print_free_slots(protocol.GetFreeSlots());
-            break;
-
-        case 's':  // Read status
-            print_free_slots(protocol.GetQueueStatus(&status));
-            print_status(status);
-            break;
-
-        case 'w':
-            ReadSegmentSteps(&terminal, &segment);
-            SendSegment(segment, &protocol);
-            break;
-
-        case 'W':
-            // Send previously read segment.
-            SendSegment(segment, &protocol);
-            break;
-
-        case 'q':
-        case 'Q':
-        case 0x04:  // Ctrl-D
-        case 0x1b:  // Escape
-            fprintf(stderr, "Signing off; Have a wonderful day!\n");
-            return 0;
-
-        default:
-            fprintf(stderr, "input key \e[1;31m0x%02X\e[0m: \e[1;33m%s\e[0m\n", key, nothing_to_do);
-        }
-    }
+  }
 }
--- host/spi.cc
+++ host/spi.cc
@@ -10,75 +10,82 @@
 
 #include <algorithm>
 
+#include "../sim/hsg-sim.h"
+
 // Report io-error, return false.
 static bool io_problem(const char *msg) {
-    perror(msg);
-    return false;
+  perror(msg);
+  return false;
 }
 
 static void hex_dump(const void *src, size_t length, const char *prefix) {
-    const uint8_t *address = (const unsigned char*) src;
+  const uint8_t *address = (const unsigned char*) src;
 
-    printf("%s | ", prefix);
-    if (!src) {
-        printf("\e[1;31m(ignored)\e[0m\n");
-        return;
-    }
-    if (length == 0) {
-        printf("\e[1;31m(no bytes)\e[0m\n");
-        return;
-    }
-    for (size_t b = 0; b < length; ++b) {
-        printf("%02X ", address[b]);
-    }
-    printf(" | ");
-    for (size_t b = 0; b < length; ++b) {
-        uint8_t c = address[b];
-        printf("%c", (c < 33 || c == 255) ? '.' : c);
-    }
-    printf("\e[0m\n");
+  printf("%s | ", prefix);
+  if (!src) {
+    printf("\e[1;31m(ignored)\e[0m\n");
+    return;
+  }
+  if (length == 0) {
+    printf("\e[1;31m(no bytes)\e[0m\n");
+    return;
+  }
+  for (size_t b = 0; b < length; ++b) {
+    printf("%02X ", address[b]);
+  }
+  printf(" | ");
+  for (size_t b = 0; b < length; ++b) {
+    uint8_t c = address[b];
+    printf("%c", (c < 33 || c == 255) ? '.' : c);
+  }
+  printf("\e[0m\n");
 }
 
 SPIHost::~SPIHost() { close(fd_); }
 
 bool SPIHost::Connect(const char *device, const Options &set_options) {
-    if (fd_ >= 0)
-        close(fd_);  // Was already open.
-    if ((fd_ = open(device, O_RDWR)) < 0)
-        return io_problem(device);
+  options_ = set_options;
+  if (spi_sim_) return true;
 
-    options_ = set_options;
-    options_.mode = 0x55555555;  // See if we get properly set.
-    // Right now, we only read the mode, but later should allow to set
-    // it via options.
-    int ret = ioctl(fd_, SPI_IOC_RD_MODE32, &options_.mode);
-    if (ret < 0) return io_problem("Couldn't read mode");
-    fprintf(stderr, "SPI mode is: 0x%08x\n", options_.mode);
+  if (fd_ >= 0)
+    close(fd_);  // Was already open.
+  if ((fd_ = open(device, O_RDWR)) < 0)
+    return io_problem(device);
 
-    ret = ioctl(fd_, SPI_IOC_WR_BITS_PER_WORD, &options_.bits_per_word);
-    if (ret < 0) return io_problem("Setting bits per word");
+  options_.mode = 0x55555555;  // See if we get properly set.
+  // Right now, we only read the mode, but later should allow to set
+  // it via options.
+  int ret = ioctl(fd_, SPI_IOC_RD_MODE32, &options_.mode);
+  if (ret < 0) return io_problem("Couldn't read mode");
+  fprintf(stderr, "SPI mode is: 0x%08x\n", options_.mode);
 
-    // Let's read back to see that we set what we thought.
-    ret = ioctl(fd_, SPI_IOC_RD_BITS_PER_WORD, &options_.bits_per_word);
-    if (ret < 0) return io_problem("Couldn't read bits per word");
-    if (options_.bits_per_word != set_options.bits_per_word)
-        return io_problem("Bits per word didn't stick");
+  ret = ioctl(fd_, SPI_IOC_WR_BITS_PER_WORD, &options_.bits_per_word);
+  if (ret < 0) return io_problem("Setting bits per word");
 
-    ret = ioctl(fd_, SPI_IOC_WR_MAX_SPEED_HZ, &options_.speed_hz);
-    if (ret < 0) return io_problem("Setting speed");
+  // Let's read back to see that we set what we thought.
+  ret = ioctl(fd_, SPI_IOC_RD_BITS_PER_WORD, &options_.bits_per_word);
+  if (ret < 0) return io_problem("Couldn't read bits per word");
+  if (options_.bits_per_word != set_options.bits_per_word)
+    return io_problem("Bits per word didn't stick");
 
-    ret = ioctl(fd_, SPI_IOC_RD_MAX_SPEED_HZ, &options_.speed_hz);
-    if (ret < 0) return io_problem("Reading speed");
-    if (options_.speed_hz != set_options.speed_hz) {
-        fprintf(stderr, "Wanted speed: %.3fkHz, actual speed: %.3fkHz\n",
-                set_options.speed_hz/1000.0, options_.speed_hz/1000.0);
-    }
-    return true;
+  ret = ioctl(fd_, SPI_IOC_WR_MAX_SPEED_HZ, &options_.speed_hz);
+  if (ret < 0) return io_problem("Setting speed");
+
+  ret = ioctl(fd_, SPI_IOC_RD_MAX_SPEED_HZ, &options_.speed_hz);
+  if (ret < 0) return io_problem("Reading speed");
+  if (options_.speed_hz != set_options.speed_hz) {
+    fprintf(stderr, "Wanted speed: %.3fkHz, actual speed: %.3fkHz\n",
+            set_options.speed_hz/1000.0, options_.speed_hz/1000.0);
+  }
+  return true;
 }
 
 
 bool SPIHost::TransferBuffer(const void *send, void *receive, size_t len,
                              bool is_last_in_transaction) {
+  if (spi_sim_) {
+    spi_sim_->SendReceive(send, receive, len, is_last_in_transaction);
+  } else {
     struct spi_ioc_transfer tr = {};
     tr.tx_buf = (unsigned long)send;
     tr.rx_buf = (unsigned long)receive;
@@ -89,25 +96,26 @@ bool SPIHost::TransferBuffer(const void *send, void *receive, size_t len,
     tr.cs_change = is_last_in_transaction ? 0 : 1;
 
     if (options_.mode & SPI_TX_QUAD)
-        tr.tx_nbits = 4;
+      tr.tx_nbits = 4;
     else if (options_.mode & SPI_TX_DUAL)
-        tr.tx_nbits = 2;
+      tr.tx_nbits = 2;
     if (options_.mode & SPI_RX_QUAD)
-        tr.rx_nbits = 4;
+      tr.rx_nbits = 4;
     else if (options_.mode & SPI_RX_DUAL)
-        tr.rx_nbits = 2;
+      tr.rx_nbits = 2;
     if (!(options_.mode & SPI_LOOP)) {
-        if (options_.mode & (SPI_TX_QUAD | SPI_TX_DUAL))
-            tr.rx_buf = 0;
-        else if (options_.mode & (SPI_RX_QUAD | SPI_RX_DUAL))
-            tr.tx_buf = 0;
+      if (options_.mode & (SPI_TX_QUAD | SPI_TX_DUAL))
+        tr.rx_buf = 0;
+      else if (options_.mode & (SPI_RX_QUAD | SPI_RX_DUAL))
+        tr.tx_buf = 0;
     }
 
     if (ioctl(fd_, SPI_IOC_MESSAGE(1), &tr) < 0)
-        return io_problem("Sending SPI message");
-    if (options_.verbose) {
-        hex_dump(send, len, "\t\e[1;36mTX");
-        hex_dump(receive, len, "\t\e[1;33mRX");
-    }
-    return true;
+      return io_problem("Sending SPI message");
+  }
+  if (options_.verbose) {
+    hex_dump(send, len, "\t\e[1;36mTX");
+    hex_dump(receive, len, "\t\e[1;33mRX");
+  }
+  return true;
 }
